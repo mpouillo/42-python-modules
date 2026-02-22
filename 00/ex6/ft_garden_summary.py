5e4dd0e8a9d6ae8a0b75bb7b374b134a6d784f7c
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-02-22 09:48:40 +0100

    updates to 00 for push to vogsphere

--- 00/ex6/ft_garden_summary.py
+++ 00/ex6/ft_garden_summary.py
@@ -1,4 +1,6 @@
 def ft_garden_summary() -> None:
     garden = input("Enter garden name: ")
     plants = input("Enter number of plants: ")
-    print(f"Garden: {garden}\nPlants: {plants}\nStatus: Growing well!")
+    print(f"Garden: {garden}\n"
+          f"Plants: {plants}\n"
+          "Status: Growing well!")
--- 00/ex7/ft_seed_inventory.py
+++ 00/ex7/ft_seed_inventory.py
@@ -1,10 +1,10 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     print(f"{seed_type.capitalize()} seeds: ", end="")
     if unit == "packets":
-        print(f"{quantity} packets available")
+        print(f"{quantity} packet{'s' if quantity > 1 else ''} available")
     elif unit == "grams":
-        print(f"{quantity} grams total")
+        print(f"{quantity} gram{'s' if quantity > 1 else ''} total")
     elif unit == "area":
-        print(f"covers {quantity} square meters")
+        print(f"covers {quantity} square meter{'s' if quantity > 1 else ''}")
     else:
         print("Unknown unit type")
